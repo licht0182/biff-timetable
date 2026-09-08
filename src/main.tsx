811fb74af21c952d82fd0ffd4d438c1973879521
licht0182 <kiriko2222@gmail.com>
2026-09-08 17:12:32 +0900

    Remove timetable click-to-remove behavior

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App'
 import './styles.css'
 import './nav-fix.css'
 import './features.css'
+import './timetable-readonly.css'
+import './timetable-readonly'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
