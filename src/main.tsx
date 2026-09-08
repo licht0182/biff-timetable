2cd8907265b0ecc40692b98caa8451b873f0a68f
licht0182 <kiriko2222@gmail.com>
2026-09-09 04:26:12 +0900

    Load timetable display formatting

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,13 @@ import './png-export.css'
 import './png-ios.css'
 import './settings.css'
 import './readability.css'
+import './timetable-display.css'
 import './timetable-readonly'
 import './png-export'
 import './settings'
 import './ui-consistency'
 import './ticket-defaults'
+import './timetable-display'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
