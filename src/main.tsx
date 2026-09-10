cfa6bd6a947b12ab322aae9936a89a53661bc948
licht0182 <kiriko2222@gmail.com>
2026-09-10 13:22:42 +0900

    Enable timetable more menu

--- src/main.tsx
+++ src/main.tsx
@@ -19,9 +19,11 @@ import './conflict-style.css'
 import './custom-events.css'
 import './timetable-runtime-layout.css'
 import './timetable-viewport-stability.css'
+import './timetable-more-menu.css'
 import './timetable-display'
 import './timetable-runtime-layout'
 import './timetable-viewport-stability'
+import './timetable-more-menu'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
