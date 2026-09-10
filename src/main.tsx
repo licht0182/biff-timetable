ca76231b91818379e7f7ffc627b4d2b8e497a681
licht0182 <kiriko2222@gmail.com>
2026-09-10 13:12:11 +0900

    Wire stable timetable viewport runtime

--- src/main.tsx
+++ src/main.tsx
@@ -18,8 +18,10 @@ import './film-modal-center.css'
 import './conflict-style.css'
 import './custom-events.css'
 import './timetable-runtime-layout.css'
+import './timetable-viewport-stability.css'
 import './timetable-display'
 import './timetable-runtime-layout'
+import './timetable-viewport-stability'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
